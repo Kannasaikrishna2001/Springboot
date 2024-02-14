09e10f4e5d54602842a8d6b9a430dc18a3b4f218
nasir.rashid_isteer <nasirganie193201@gmail.com>
2024-02-14 17:31:37 +0530

    DcmUtility class code changes

--- ProjectTemplate/ProjectTemplate/src/main/java/com/isteer/dcm/constants/DCMConstants.java
+++ ProjectTemplate/ProjectTemplate/src/main/java/com/isteer/dcm/constants/DCMConstants.java
@@ -8,11 +8,18 @@ public interface DCMConstants {
     String NOT_FOUND = "404";
     String Method_Not_Allowed = "405";
     String INTERNAL_SERVER_ERROR = "500";
-    //String placed,String inprogress, String shipped
     String ORDERSTATUS_PLACED="PLACED";
     String ORDERSTATUS_INPROGRESS="INPROGRESS";
     String ORDERSTATUS_SHIPPED="SHIPPED";
     String ORDERSTATUS_COMPLETED="COMPLETED";
     String ORDERSTATUS_NEW="NEW";
     String ORDER_MSG_SUCCESS="ORDER PLACED SUCCESSFULLY";
+    String MAIL_SMTP_HOST = "mail.smtp.host";
+    String MAIL_SMTP_PORT = "mail.smtp.port";
+    String MAIL_SMTP_AUTH = "mail.smtp.auth";
+    String MAIL_SMTP_SOCKETFACTORY_CLASS = "mail.smtp.socketFactory.class";
+    String MAIL_SMTP_SOCKETFACTORY_FALLBACK = "mail.smtp.socketFactory.fallback";
+    String JAVAX_NET_SSLFACTORY = "javax.net.ssl.SSLSocketFactory";
+    String EMAIL_SUCCESS_MESSAGE="Message sent successfully";
+    String EMAIL_ERROR_MESSAGE="Error while sending email";
 }
--- ProjectTemplate/ProjectTemplate/src/main/java/com/isteer/dcm/utility/DcmUtility.java
+++ ProjectTemplate/ProjectTemplate/src/main/java/com/isteer/dcm/utility/DcmUtility.java
@@ -1,6 +1,9 @@
 package com.isteer.dcm.utility;
 
+import com.isteer.dcm.constants.DCMConstants;
 import com.isteer.dcm.model.Status;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.mail.Message;
 import javax.mail.PasswordAuthentication;
@@ -11,13 +14,31 @@ import javax.mail.internet.MimeMessage;
 import java.io.*;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+@Component
 public class DcmUtility {
+
+    @Value("$(smtp.server.username)")
+    private String username;
+
+    @Value("$(smtp.server.password)")
+    private String password;
+
+    @Value("$(smtp.server.hostname)")
+    private String hostname;
+
+    @Value("$(smtp.server.port)")
+    private String port;
+
+    @Value("$(smtp.server.ssl.protocols)")
+    private String sslProtocol;
+
+    @Value("$(smtp.applicaton.content.type)")
+    private String contentType;
+
     public static String getStacktraceSubString(String stackTrace) {
         String[] words = stackTrace.split("\\s+");
         StringBuilder result = new StringBuilder();
@@ -32,59 +53,41 @@ public class DcmUtility {
         return result.toString().trim();
     }
 
-    public String ZipFile() throws IOException {
-        String file1 = "D:\\business-financial-data-september-2023-quarter.csv";
-        String file2 = "D:\\machine-readable-business-employment-data-sep-2023-quarter.csv";
-        final List<String> srcFiles = Arrays.asList(file1, file2);
-
-        final FileOutputStream fos = new FileOutputStream(Paths.get(file1).getParent().toAbsolutePath() + "/SpringBootCompressed.7z");
+    public String createSevenZipFile(String sevenZipFilePath, File fileToZip) throws IOException {
+        String outputSevenZipFile = Paths.get(sevenZipFilePath) + fileToZip.getName().replace(".csv", getCurrentDateTime() + ".7z");
+        final FileOutputStream fos = new FileOutputStream(outputSevenZipFile);
         ZipOutputStream zipOut = new ZipOutputStream(fos);
-
-        for (String srcFile : srcFiles) {
-            File fileToZip = new File(srcFile);
-            FileInputStream fis = new FileInputStream(fileToZip);
-            ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
-            zipOut.putNextEntry(zipEntry);
-
-            byte[] bytes = new byte[1024];
-            int length;
-            while((length = fis.read(bytes)) >= 0) {
-                zipOut.write(bytes, 0, length);
-            }
-            fis.close();
+        FileInputStream fis = new FileInputStream(fileToZip);
+        ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+        zipOut.putNextEntry(zipEntry);
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            zipOut.write(bytes, 0, length);
         }
-
+        fis.close();
         zipOut.close();
         fos.close();
-        return null;
+        return outputSevenZipFile;
     }
 
-
     public Status sendEmail(String toEmail, String subject, String body) {
-
-        String MAIL_SMTP_HOST = "mail.smtp.host";
-        String MAIL_SMTP_PORT = "mail.smtp.port";
-        String MAIL_SMTP_AUTH = "mail.smtp.auth";
-        String MAIL_SMTP_SOCKETFACTORY_CLASS = "mail.smtp.socketFactory.class";
-        String MAIL_SMTP_SOCKETFACTORY_FALLBACK = "mail.smtp.socketFactory.fallback";
-        String JAVAX_NET_SSLFACTORY = "javax.net.ssl.SSLSocketFactory";
-
         try {
             java.util.Properties mailProperties = System.getProperties();
 
-            mailProperties.setProperty(MAIL_SMTP_HOST, "smtp.gmail.com");
-            mailProperties.setProperty(MAIL_SMTP_PORT, "465");
-            mailProperties.setProperty(MAIL_SMTP_AUTH, "true");
+            mailProperties.setProperty(DCMConstants.MAIL_SMTP_HOST, hostname);
+            mailProperties.setProperty(DCMConstants.MAIL_SMTP_PORT, port);
+            mailProperties.setProperty(DCMConstants.MAIL_SMTP_AUTH, "true");
             mailProperties.setProperty("mail.smtp.ssl.enable", "true");
-            mailProperties.setProperty(MAIL_SMTP_SOCKETFACTORY_CLASS, JAVAX_NET_SSLFACTORY);
-            mailProperties.setProperty(MAIL_SMTP_SOCKETFACTORY_FALLBACK, "true");
-            mailProperties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+            mailProperties.setProperty(DCMConstants.MAIL_SMTP_SOCKETFACTORY_CLASS, DCMConstants.JAVAX_NET_SSLFACTORY);
+            mailProperties.setProperty(DCMConstants.MAIL_SMTP_SOCKETFACTORY_FALLBACK, "true");
+            mailProperties.put("mail.smtp.ssl.protocols", sslProtocol);
 
             Session session = Session.getInstance(mailProperties, new javax.mail.Authenticator() {
 
                 @Override
                 protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication("jeevan.sy@isteer.com", "ywugkmznysyzfboz");
+                    return new PasswordAuthentication(username, password);
                 }
             });
 
@@ -92,45 +95,29 @@ public class DcmUtility {
             MimeMessage message = new MimeMessage(session);
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
             message.setSubject(subject);
-            message.setContent(body, "text/html");
+            message.setContent(body, contentType);
 
             Transport.send(message);
-            return new Status(1, "Message sent successfully");
+            return new Status(1, DCMConstants.EMAIL_SUCCESS_MESSAGE);
         } catch (Exception e) {
             System.out.println(e);
-            return new Status(0, "Error while sending email");
-        }
-    }
-
-    public class TransactionIdGenerator {
-
-        public String generateTransactionId() {
-            // Get process ID
-            String processId = getProcessId();
-
-            // Get current date and time in the specified format
-            String dateTime = getCurrentDateTime();
-
-            // Combine process ID and datetime to form the transaction ID
-            String transactionId = processId + dateTime;
-            return transactionId;
-
-        }
-
-        private String getProcessId() {
-
-            // Get process ID
-            String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
-            long processId = Long.parseLong(processName.split("@")[0]);
-            return String.valueOf(processId);
-        }
-
-        private String getCurrentDateTime() {
-            // Get current date and time in the specified format
-            SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyyHHmmssSSS");
-            return dateFormat.format(new Date());
+            return new Status(0, DCMConstants.EMAIL_ERROR_MESSAGE);
         }
     }
 
 
+    public String generateTransactionId() {
+        return getProcessId() + "_" + getCurrentDateTime();
+    }
+
+    private String getProcessId() {
+        String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
+        long processId = Long.parseLong(processName.split("@")[0]);
+        return String.valueOf(processId);
+    }
+
+    private String getCurrentDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyyHHmmssSSS");
+        return dateFormat.format(new Date());
+    }
 }
