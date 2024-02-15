af3e67ee997b4908c22a757a2ee6f9d265123f76
nasir.rashid_isteer <nasirganie193201@gmail.com>
2024-02-15 11:56:19 +0530

    Validate region and stores service

--- ProjectTemplate/ProjectTemplate/src/main/java/com/isteer/dcm/repository/RegionalStoreRepository.java
+++ ProjectTemplate/ProjectTemplate/src/main/java/com/isteer/dcm/repository/RegionalStoreRepository.java
@@ -9,11 +9,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RegionalStoreRepository extends JpaRepository<RegionalStores,Long> {
 
-    @Query(value = "SELECT region_Id, region_Name, store_Id, store_Name FROM RegionalStores WHERE region_Id = :region_Id AND region_Name = :region_Name AND store_Id = :store_Id AND store_Name = :store_Name", nativeQuery = true)
-    RegionalStores findByRegionIdAndRegionName(@Param("region_Id") int region_Id, @Param("region_Name") String region_Name, @Param("store_Id") int store_Id, @Param("store_Name") String store_Name);
+    @Query(value = "SELECT region_Id, region_Name, store_Id, store_Name FROM RegionalStores WHERE region_Id = :region_Id AND region_Name = :region_Name", nativeQuery = true)
+    RegionalStores findByRegionIdAndRegionName(@Param("region_Id") int region_Id, @Param("region_Name") String region_Name);
 
-    @Query(value = "SELECT region_Id, region_Name, store_Id, store_Name FROM RegionalStores WHERE region_Id = :region_Id AND store_Id = :store_Id", nativeQuery = true)
-    RegionalStores getRegionIdAndStoreId(@Param("region_Id") int region_Id, @Param("store_Id") int store_Id);
-}
+   }
 
 
