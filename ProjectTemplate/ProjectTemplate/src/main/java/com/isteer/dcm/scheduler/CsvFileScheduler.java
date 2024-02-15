885bfc7e90439ea18593961319737c8a9305880a
sameer <sameer.sibaragatti@isteer.com>
2024-02-15 10:39:14 +0530

    I have completed the Task(15/02/2024)

--- ProjectTemplate/ProjectTemplate/src/main/java/com/isteer/dcm/scheduler/CsvFileScheduler.java
+++ ProjectTemplate/ProjectTemplate/src/main/java/com/isteer/dcm/scheduler/CsvFileScheduler.java
@@ -1,10 +1,7 @@
 package com.isteer.dcm.scheduler;
 
-import com.isteer.dcm.entity.DcmUsers;
 import com.isteer.dcm.model.Production;
 import com.isteer.dcm.service.CsvReaderService;
-import com.isteer.dcm.service.DataInitializer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,25 +13,21 @@ public class CsvFileScheduler {
 
     private final CsvReaderService csvReaderService;
 
-    /*@Value("${csv.file.path}")
-    private String filePath;*/
+    @Value("${csv.file.path}")
+    private String filePath;
+
 
 
     public CsvFileScheduler(CsvReaderService csvReaderService) {
         this.csvReaderService = csvReaderService;
     }
-    @Autowired
-    DataInitializer dataInitializer;
 
-    @Scheduled(cron = "0 57 18 ? * WED")
+    @Scheduled(cron = "0 0 0 ? * MON")
     public void parseCsvFile() {
-        List<DcmUsers> userList=dataInitializer.getDcmUsersList();
-        System.out.println(" this is the data loaded on startup "+userList.get(0).getUserid());
-
-       /* List<Production> productions = csvReaderService.readCsvFile(filePath);
+        List<Production> productions = csvReaderService.readCsvFile(filePath);
 
         for (Production production : productions) {
             System.out.println(production);
-        }*/
+        }
     }
 }
